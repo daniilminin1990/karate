3f9cb8281718f0624b2bd05819aee99cb89921f0
Daniil <daniilminin1990@yandex.ru>
2024-10-22 14:40:59 +0500

    Updates

---
+++ assets/Button.stories-BKrXGoiU.js
@@ -0,0 +1,26 @@
+import{j as N}from"./jsx-runtime-CkxqCPlQ.js";import{r as P}from"./index-DJO9vBfz.js";import{c as S}from"./clsx-B-dksMZM.js";const B="_button_hgime_4",F="_primary_hgime_14",L="_sun_hgime_14",R="_secondary_hgime_36",j="_fullWidth_hgime_53",w="_outlined_hgime_61",E="_text_hgime_74",n={button:B,primary:F,sun:L,secondary:R,fullWidth:j,outlined:w,text:E},_=(v,b)=>{const{as:x="button",className:T,fullWidth:W,variant:q="primary",...k}=v;return N.jsx(x,{className:S(n.button,n[q],W&&n.fullWidth,T),ref:b,...k})},g=P.forwardRef(_);_.__docgenInfo={description:"",methods:[],displayName:"ButtonPolymorph",props:{as:{required:!1,tsType:{name:"T"},description:""},children:{required:!1,tsType:{name:"ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""},fullWidth:{required:!1,tsType:{name:"boolean"},description:""},variant:{required:!1,tsType:{name:"union",raw:"'outlined' | 'primary' | 'secondary' | 'text'",elements:[{name:"literal",value:"'outlined'"},{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'text'"}]},description:""}}};g.__docgenInfo={description:"",methods:[],displayName:"Button",props:{as:{required:!1,tsType:{name:"T"},description:""},children:{required:!1,tsType:{name:"ReactNode"},description:""},className:{required:!1,tsType:{name:"string"},description:""},fullWidth:{required:!1,tsType:{name:"boolean"},description:""},variant:{required:!1,tsType:{name:"union",raw:"'outlined' | 'primary' | 'secondary' | 'text'",elements:[{name:"literal",value:"'outlined'"},{name:"literal",value:"'primary'"},{name:"literal",value:"'secondary'"},{name:"literal",value:"'text'"}]},description:""}}};const O={argTypes:{variant:{control:{type:"radio"},options:["primary","secondary"]}},component:g,tags:["autodocs"],title:"Components/Button"},e={args:{children:"Primary",disabled:!1,variant:"primary"}},r={args:{children:"Secondary",disabled:!1,variant:"secondary"}},a={args:{children:"Full Width Primary Button",disabled:!1,fullWidth:!0,variant:"primary"}},t={args:{as:"a",children:"Link that looks like a button",variant:"primary"}};var s,i,o;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`{
+  args: {
+    children: 'Primary',
+    disabled: false,
+    variant: 'primary'
+  }
+}`,...(o=(i=e.parameters)==null?void 0:i.docs)==null?void 0:o.source}}};var l,d,c;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`{
+  args: {
+    children: 'Secondary',
+    disabled: false,
+    variant: 'secondary'
+  }
+}`,...(c=(d=r.parameters)==null?void 0:d.docs)==null?void 0:c.source}}};var m,u,p;a.parameters={...a.parameters,docs:{...(m=a.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  args: {
+    children: 'Full Width Primary Button',
+    disabled: false,
+    fullWidth: true,
+    variant: 'primary'
+  }
+}`,...(p=(u=a.parameters)==null?void 0:u.docs)==null?void 0:p.source}}};var y,h,f;t.parameters={...t.parameters,docs:{...(y=t.parameters)==null?void 0:y.docs,source:{originalSource:`{
+  args: {
+    as: 'a',
+    children: 'Link that looks like a button',
+    variant: 'primary'
+  }
+}`,...(f=(h=t.parameters)==null?void 0:h.docs)==null?void 0:f.source}}};const z=["Primary","Secondary","FullWidth","AsLink"];export{t as AsLink,a as FullWidth,e as Primary,r as Secondary,z as __namedExportsOrder,O as default};
